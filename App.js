bb9dfbe59107026e3fb1463508c6fca858dddf64
AVATAR197 <39923116+AVATAR197@users.noreply.github.com>
2019-10-30 10:21:35 +0100

    initial backup

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Calendar from './lib/Calendar';
+
+export default function App() {
+  return (
+      <Calendar />
+  );
+}
